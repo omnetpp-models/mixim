1c22b4806fb3676c47a9e4326d0e6147ae4794c9
Levente Mészáros <levente.meszaros@gmail.com>
2011-08-01 13:39:21 +0200

    Deleted the Blackboard tests.
    The Blackboard is not used anymore, because it has been replaced with
    the standard signaling mechanism or the NotificationBoard of INET.

--- tests/blackboard/runTest.sh
+++
@@ -1,7 +0,0 @@
-#/bin/bash
-
-./blackboard > out.tmp
-
-diff -I '^Assigned runID=' -I '^Loading NED files from' -w exp-output out.tmp
-
-rm -f out.tmp
--- tests/blackboard/updateExpOutput.sh
+++
@@ -1,4 +0,0 @@
-#/bin/bash
-
-./blackboard > exp-output
-
