de71d350168f6fc8f7ea7bfe80a9481da078f269
Levente Mészáros <levente.meszaros@gmail.com>
2011-09-01 15:59:24 +0200

    Deleted obsolete mobility tests.
    These tests were not yet added to INET.

--- tests/baseMobility/runTest.sh
+++
@@ -1,7 +0,0 @@
-#/bin/bash
-
-./baseMobility -c Test1 > out.tmp
-
-diff -I '^Assigned runID=' -I '^Loading NED files from' -w exp-output out.tmp
-
-rm -f out.tmp
--- tests/baseMobility/updateExpOutput.sh
+++
@@ -1,4 +0,0 @@
-#/bin/bash
-
-./baseMobility -c Test1 > exp-output
-
